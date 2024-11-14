41d94a437d31af8ed82f6e9a66ce802d39b6b630
seba ibraheem <sebaalmahdy@gmail.com>
2024-11-15 00:44:13 +0200

    take the file's name from the user is added

--- main.cpp
+++ main.cpp
@@ -7,30 +7,42 @@ using namespace std;
 int main()
 {
     string s = "file.txt";
-    ifstream file("file.txt" );
-    string word;
-    map<string ,int>mp;
-    while(file>>word)
-    {
-        string corrected_word;
-        for(char c:word)
-        {
-            if(isalnum(c))
-            {
-                c=tolower(c);
-                corrected_word+=c;
-            }
-            if(c=='-')
-            {
-                corrected_word+=c;
-            }
-        }
-        mp[corrected_word]++;
+    string file_name;
+    cout<<"please enter the file's name: ";
+    cin>>file_name;
 
-    }
-    for (auto i : mp)
+    ifstream file(file_name );
+    if(!file)
     {
-        cout<<i.first<<" : "<<i.second<<endl;
+        cout<<"error in opening the file"<<endl;
     }
+    else
+    {
+        string word;
+        map<string ,int>mp;
+        while(file>>word)
+        {
+            string corrected_word;
+            for(char c:word)
+            {
+                if(isalnum(c))
+                {
+                    c=tolower(c);
+                    corrected_word+=c;
+                }
+                if(c=='-')
+                {
+                    corrected_word+=c;
+                }
+            }
+            mp[corrected_word]++;
+
+        }
+        for (auto i : mp)
+        {
+            cout<<i.first<<" : "<<i.second<<endl;
+        }
+    }
+
 
 }
